90da32530e07419047e72a9693f2bf1f54f55d70
ruannyury <ruannyury1@outlook.com>
2021-06-23 16:51:26 -0300

    Version 1.0

--- Atividades[Avaliação] - Etapa 2 /app/__init__.py
+++ Atividades[Avaliação] - Etapa 2 /app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,8 +9,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-lm = LoginManager()
-lm.init_app(app)
 
 from app.models import tables
 from app.controllers import default
--- Atividades[Avaliação] - Etapa 2 /app/controllers/default.py
+++ Atividades[Avaliação] - Etapa 2 /app/controllers/default.py
@@ -2,25 +2,13 @@ from app import app, db
 from flask import render_template
 from app.models.tables import User
 
-from app.models.forms import LoginForm
+# from app.models.forms import LoginForm
 
 
 @app.route("/index")
 @app.route("/")
 def index():
     return render_template('index.html')
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
-    else:
-        print(form.errors)
-    return render_template('login.html',
-                           form=form)
 
 
 @app.route("/teste/<info>")
