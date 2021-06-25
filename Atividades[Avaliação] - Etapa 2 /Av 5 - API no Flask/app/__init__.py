f2350b892610b31827eaf538ab43cf0cebefe88a
ruannyury <ruannyury1@outlook.com>
2021-06-25 19:32:42 -0300

    Commit.

--- Atividades[Avaliação] - Etapa 2 /Av 5 - API no Flask/app/__init__.py
+++ Atividades[Avaliação] - Etapa 2 /Av 5 - API no Flask/app/__init__.py
@@ -5,15 +5,15 @@ from app.models.tables import Cliente
 app = Flask(__name__)
 app.config.from_object('config')
 
-global_id = 4
-global_codigo = 400
+idGeralCliente = 4
+codigoGeralCliente = 400
 
 
-def atualizar_1(cliente):
-    global global_id
-    global global_codigo
-    global_id += 1
-    global_codigo += 100
+def refresh1(cliente):
+    global idGeralCliente
+    global codigoGeralCliente
+    idGeralCliente += 1
+    codigoGeralCliente += 100
     clientes.append(cliente)
 
 
@@ -27,6 +27,7 @@ clientes = [Cliente(1, "Ruann Yury", 100, '200.100.345-34', 'fisica'),
             Cliente(3, "Rafolas", 300, '200.100.347-34', 'juridica')]
 
 
+@app.route("/PaginaInicial")
 @app.route("/home")
 @app.route("/index")
 @app.route("/")
@@ -34,39 +35,49 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/adicionar", methods=['GET', 'POST'])
+@app.route("/create", methods=['GET', 'POST'])
 def adicionar():
     if request.method == 'POST':
-        cliente = Cliente(global_id, request.form['nome'], global_codigo, request.form['cpfcnpj'], 'fisica')
-        atualizar_1(cliente)
-        flash('Adicionado!')
-        return redirect(url_for('visualizar'))
-    return render_template('adicionar.html')
+        if request.form['fisijurid'] == '1':
+            cliente = Cliente(idGeralCliente, request.form['nome'], codigoGeralCliente, request.form['cpfcnpj'], 'P. Física')
+            refresh1(cliente)
+            flash('Adicionado!')
+            return redirect(url_for('visualizar'))
+        elif request.form['fisijurid'] == '2':
+            cliente = Cliente(idGeralCliente, request.form['nome'], codigoGeralCliente, request.form['cpfcnpj'], 'P. Jurídica')
+            refresh1(cliente)
+            flash('Adicionado!')
+            return redirect(url_for('visualizar'))
+        else:
+            flash('Escolha direito!')
+            return redirect(url_for('adicionar'))
+    return render_template('create.html')
 
 
-@app.route("/visualizar")
+@app.route("/read")
 def visualizar():
     return render_template('visualizar.html', clientes=clientes)
 
 
-@app.route("/atualizar", methods=['GET', 'POST'])
+@app.route("/update", methods=['GET', 'POST'])
 def atualizar():
     if request.method == 'POST':
         cliente_id = int(request.form['id'])
         cliente = [c for c in clientes if cliente_id == c.get_id()][0]
         cliente.set_nome(request.form['nome'])
         cliente.set_cnpjcpf(request.form['cpfcnpj'])
-        flash('Atualizado!')
+        flash('Atualizado com sucesso!')
         return redirect(url_for('visualizar'))
     return render_template('atualizar.html')
 
 
-@app.route("/remover", methods=['GET', 'POST'])
+@app.route("/delete", methods=['GET', 'POST'])
 def remover():
     if request.method == 'POST':
         cliente_id = int(request.form['id'])
         cliente = [c for c in clientes if cliente_id == c.get_id()][0]
         deletar(cliente)
-        return render_template('remover.html', cliente=cliente)
+        flash('Removido com sucesso, seu bosta!')
+        return redirect(url_for('visualizar'))
     else:
         return render_template('remover.html')
